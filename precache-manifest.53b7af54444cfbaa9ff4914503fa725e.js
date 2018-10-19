1fcd841076139ead4ca5764b9740617f3f984a97
Greg s Pask <gregp63@hotmail.co.uk>
2018-10-19 22:12:37 +0100

    Updates

---
+++ precache-manifest.53b7af54444cfbaa9ff4914503fa725e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f98c74e2d275b2f70b3b",
+    "url": "/React-JS---Game-of-Life/static/js/runtime~main.f98c74e2.js"
+  },
+  {
+    "revision": "d69659f1a24333ca257d",
+    "url": "/React-JS---Game-of-Life/static/js/main.d69659f1.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/React-JS---Game-of-Life/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "d69659f1a24333ca257d",
+    "url": "/React-JS---Game-of-Life/static/css/main.7001da19.chunk.css"
+  },
+  {
+    "revision": "93fec0ecb926e8e8b0f1cca6b8b34b6b",
+    "url": "/React-JS---Game-of-Life/index.html"
+  }
+];
